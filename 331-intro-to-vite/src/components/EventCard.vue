97e261a44eb03a0fef5eb2b4f2edbe3261cb4041
zbxxhwyh <zhangbaixuan2022@outlook.com>
2024-10-24 09:37:20 +0800

    8

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
-//defineProps<{
-//  msg: string
-//}>()
+import { ref } from 'vue'
+import type { Event } from '@/types'
+defineProps<{
+  event: Event
+}>()
+// const event = ref({
+//   id: 5928101,
+//   category: 'animal welfare',
+//   title: 'Cat Adoption Day',
+//   description: 'Find your new feline friend at this event.',
+//   location: 'Meow Town',
+//   date: 'January 28, 2022',
+//   time: '12:00',
+//   petsAllowed: true,
+//   organizer: 'Kat Laydee'
+// })
 </script>
 
 <template>
@@ -13,7 +26,10 @@
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
   </div> -->
-  <div class="greetings"></div>
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
 </template>
 
 <!-- <style scoped>
@@ -40,3 +56,16 @@ h3 {
   }
 }
 </style> -->
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
